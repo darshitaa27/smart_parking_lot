2790777da2e0f8047490deff52717231e448226b
Pratham598 <Prao3607@conestogac.on.ca>
2025-11-07 23:37:23 -0500

    Initialize dashboardview model.

--- smart_parking_lot/Smart_Parking_Lot/ViewModels/DashboardViewModel.cs
+++ smart_parking_lot/Smart_Parking_Lot/ViewModels/DashboardViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using SmartParkingSystem.Models;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
-namespace Smart_Parking_Lot.ViewModels
+namespace SmartParkingSystem.ViewModels 
 {
-    internal class DashboardViewModel
+    public class DashboardViewModel : INotifyPropertyChanged
     {
+        public EntranceSensor EntranceSensor { get; private set; } = new EntranceSensor();
+        public GateBarrierActuator Gate { get; private set; } = new GateBarrierActuator();
+        public SlotSensor SlotSensor { get; private set; } = new SlotSensor(10);
+        public LightingController Lights { get; private set; } = new LightingController();
+
+        private DispatcherTimer refreshTimer;
     }
 }
